cef8325a5923d43d0723ee2efc2d963a8dd504d1
zthxxx <zthxxx.me@gmail.com>
2019-10-03 16:47:00 +0800

    fix: dependency failback to zsh default history search

--- zsh-history-enquirer.plugin.zsh
+++ zsh-history-enquirer.plugin.zsh
@@ -1,13 +1,19 @@
 # replace '^R' search of zsh-history-enquirer in zsh
 
-# fcenquire is bin name of zsh-history-enquirer
-if command -v fcenquire > /dev/null; then
-  function history_enquire() {
+function history_enquire() {
+  # fcenquire is the bin name of zsh-history-enquirer
+  if [[ $commands[fcenquire] ]]; then
     BUFFER=$(fcenquire "$LBUFFER")
     CUOSOR=$#BUFFER
     zle -R -c
-  }
+  else
+    # fallback to use zsh default history search
+    # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#History-Control
+    bindkey '^R' history-incremental-search-backward
+    zle history-incremental-search-backward
+    bindkey '^R' history_enquire
+  fi
+}
 
-  zle -N history_enquire
-  bindkey '^R' history_enquire
-fi
+zle -N history_enquire
+bindkey '^R' history_enquire
