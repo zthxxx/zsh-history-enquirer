8591183842731db84edacba66045353d3a15113c
zthxxx <zthxxx.me@gmail.com>
2021-10-16 14:32:00 +0800

    fix: fix install script for .zshrc as symlink

--- scripts/postinstall.zsh
+++ scripts/postinstall.zsh
@@ -17,5 +17,12 @@ mkdir -p ${plugins_dir}/${package_name}
 
 ln -fs "`pwd`/scripts/${package_name}.plugin.zsh" "${plugins_dir}/${package_name}/"
 
-perl -i -pe "s/^[ \t]*${package_name}[ \t\n]*//gms" ${HOME}/.zshrc
-perl -i -pe "s/^[ \t]*plugins=\(/plugins=(\n  ${package_name}\n/gms" $HOME/.zshrc
+# it's same as `realpath`, but `realpath` is GNU only and not builtin
+prel-realpath() {
+  perl -MCwd -e 'print Cwd::realpath($ARGV[0]),qq<\n>' $1
+}
+
+local zsh_config_file="$(prel-realpath ${HOME}/.zshrc)"
+
+perl -i -pe "s/^[ \t]*${package_name}[ \t\n]*//gms" "${zsh_config_file}"
+perl -i -pe "s/^[ \t]*plugins=\(/plugins=(\n  ${package_name}\n/gms" "${zsh_config_file}"
